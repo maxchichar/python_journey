b7abe31164691acc7e3972cb1861b82a74643686
maxchichar <maxwellchibuezecharles@gmail.com>
2026-08-05 15:21:07 +0100

    Fix Age Math

--- Tn_drills/drill05.py
+++ Tn_drills/drill05.py
@@ -3,3 +3,11 @@ def fix_greeting(name):
     return f"Hello, {name}."
 
 print(fix_greeting("Maxwell"))
+
+# Fix Age Math
+def next_age(age_text):
+    age = int(age_text)
+    next_year = age + 1
+    return next_year
+
+print(next_age("18"))
